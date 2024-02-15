64351382733db29c07212c60741a278f3c6d6460
amardeep <amardeepjk@gmail.com>
2024-02-16 06:24:16 +0800

    wip

--- main.cpp
+++ main.cpp
@@ -3,32 +3,8 @@
 #include <cereal/archives/json.hpp>
 #include <cereal/types/vector.hpp>
 
-struct MyRecord {
-    uint8_t x, y;
-    float z;
 
-    template<class Archive>
-    void serialize(Archive &ar) {
-        ar(x, y, z);
-    }
-};
 
-struct SomeData {
-    int32_t id;
-    std::shared_ptr<std::unordered_map<uint32_t, MyRecord>> data;
-
-    template<class Archive>
-    void save(Archive &ar) const {
-        ar(data);
-    }
-
-    template<class Archive>
-    void load(Archive &ar) {
-        static int32_t idGen = 0;
-        id = idGen++;
-        ar(data);
-    }
-};
 
 
 int main() {
