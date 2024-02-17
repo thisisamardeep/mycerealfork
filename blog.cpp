8853a476f92437bf100209f726b3a2175c0df190
amardeep <amardeepjk@gmail.com>
2024-02-17 10:13:13 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -1,9 +1,23 @@
 //
 // Created by amardeep on 17/02/24.
 //
+#include "iostream"
+
+char rtt1(int y) {
+    return y;
+}
 
 int main() {
+    char (*rtt)(int);
+//    char rtt1(int);
+    rtt = rtt1;
+//    std::string test1{};
+//    std::string tes1t1();
+//    std::cout << tes1t1.empty() << std::endl;
+//    auto er = amar;
 
+    auto rr = rtt(222);
+    std::cout << rr << std::endl;
 
     return 0;
 }
