387d499ff8b0b7449dad161ef7fb3620e149c36c
amardeep <amardeepjk@gmail.com>
2024-02-18 07:26:46 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -1,23 +1,52 @@
 //
 // Created by amardeep on 17/02/24.
 //
+#include "type_traits"
 #include "iostream"
 
-char rtt1(int y) {
-    return y;
+class MyInteger {
+
+public:
+    MyInteger(int t) : m_i(t) {
+
+    }
+
+public:
+    int m_i;
+
+};
+
+class Square {
+public:
+    Square(MyInteger i) : my_int(i) {
+    }
+
+    int squared() {
+        return my_int.m_i * my_int.m_i;
+    }
+
+public:
+    MyInteger my_int;
+};
+
+int MyInteg() {
+    return 56;
 }
+
+
+
+//    MyInteger rr{34};
+//MyInteger mi(i);
+////
+////    Square r(mi);
+//Square r(MyInteger(i));
+
+
+//typename MyInteg MyIntegrr;
 
 int main() {
-    char (*rtt)(int);
-//    char rtt1(int);
-    rtt = rtt1;
-//    std::string test1{};
-//    std::string tes1t1();
-//    std::cout << tes1t1.empty() << std::endl;
-//    auto er = amar;
-
-    auto rr = rtt(222);
-    std::cout << rr << std::endl;
-
-    return 0;
+    std::cout << "std::is_function<MyInteger>    " << std::boolalpha << std::is_function<MyInteger>::value << std::endl;
+    std::cout << "std::is_function<MyInteger()>    " << std::boolalpha << std::is_function<MyInteger()>::value
+              << std::endl;
 }
+
